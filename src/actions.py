bfd6bb3e16d036fa9d4236ab38ade431367a9181
Eugene Lin <eugenelin@adminlocalsmbp.1800gotjunk.com>
2017-04-26 16:39:10 -0700

    test

--- src/actions.py
+++ src/actions.py
@@ -9,7 +9,7 @@ def intention_func1(sender_id, parameters):
     msg = 'jf1 rocks! '
     resp_data = {"id":sender_id,"message":msg} #
     #res = requests.post(os.environ['JFACE_URL'], json=resp_data)
-    fb.send_message(sender_id, msg)
+    res = fb.send_message(sender_id, msg)
     return res
 
 def begin_dialog(sender_id, parameters):
