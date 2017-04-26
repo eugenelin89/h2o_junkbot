65abbcd2b030a69c046f4559ee565102b468fe88
Eugene Lin <eugenelin@adminlocalsmbp.1800gotjunk.com>
2017-04-26 16:51:43 -0700

    test

--- src/actions.py
+++ src/actions.py
@@ -6,7 +6,7 @@ def intention_func1(sender_id, parameters):
     data = {}
     # at this layer, should be platform agnostic.
     # send msg back to user
-    msg = 'jf1 rocks! '
+    msg = 'foobar rocks!'
     resp_data = {"id":sender_id,"message":msg} #
     #res = requests.post(os.environ['JFACE_URL'], json=resp_data)
     res = fb.send_message(sender_id, msg)
--- src/intents.py
+++ src/intents.py
@@ -2,6 +2,7 @@ from actions import *
 
 
 intents = {
+'Default Fallback Intent':intention_func1,
 'Fallback':intention_func1,
 'Hi':begin_dialog
 }
