356151a02fef5f429d268c36532f329cfb1a4018
Eugene Lin <eugenelin@adminlocalsmbp.1800gotjunk.com>
2017-04-28 16:40:08 -0700

    get fb profile

--- src/fb.py
+++ src/fb.py
@@ -15,5 +15,10 @@ def get_fb_profile(sender_id):
     url = os.environ['FB_GRAPHAPI_URL']+sender_id
     params = {'fields':'first_name,last_name,profile_pic,locale,timezone,gender',
     'access_token':os.environ['FB_ACCESS_TOKEN']}
+    # For local testing:
+    #url = 'https://graph.facebook.com/v2.6/'+sender_id
+    #params = {'fields':'first_name,last_name,profile_pic,locale,timezone,gender',
+    #'access_token':'EAAaZCTAZBYPfoBAF0ZC9ZBfZCCfzEHfBeOZAxU9u0PvQIWt6D3JxfBE4PaKEVelakJ8A45LkhTn802mhSiD3YvvjZAEdw2584QOZCLXhns9rqFTVZCc2SYgqcXK7JwjR394J2dZCrt6xhMfVsZBPpIIDCr5x8laKHElTUsgYj3luZBpJMwZDZD'}
+
     res = requests.get(url, params).json()
-    return res 
+    return res
