32b6e9c4f3a1ee9b99d6a1e75a7a37d72bc8aeb4
Eugene <eugene@Eugenes-iMac.local>
2017-04-29 20:10:43 -0700

    test

--- src/tasks.py
+++ src/tasks.py
@@ -25,7 +25,7 @@ def fb_process(data):
         timestamp = data['entry'][0]['time']
         apiai_query = apiai.query(sender_id, message)
         apiai_action = apiai_query['result']['action']
-        apiai_intent = apiai_query['result']['metadata'].get('intentName')
+        apiai_intent = apiai_query['result'].get('metadata').get('intentName')
         #apiai_parameters =
         #apiai_fulfillment_msg =
         print 'API.AI Query Result: %s' % (json.dumps(apiai_query, indent = 4))
