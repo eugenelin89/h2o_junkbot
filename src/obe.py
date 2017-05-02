7c222720d682b9b16e85fecb26e3882ba8d5867d
Eugene Lin <eugenelin@Eugenes-MacBook-Pro.local>
2017-05-01 21:49:01 -0700

    verifying zip testing

--- src/obe.py
+++ src/obe.py
@@ -1,23 +1,27 @@
 import os, requests
 
-def verify_zip(zip_code):
+def is_zip_verified(zip_code):
     # Authenticate
+    verified = False
     auth = authenticate()
     if auth['error']:
-        return
+        return verified
     access_token = auth['access_token']
     instance_url = auth['instance_url']
-
-    instance_url + os.environ['OBE_RESOURCE_PATH']
-
+    url = instance_url + os.environ['OBE_RESOURCE_PATH']
     headers = {
         "Authorization":'Bearer '+access_token
     }
-
-    data = {
+    params = {
         'from_postal_code':zip_code
         'brand':os.environ['OBE_BRAND']
     }
+    res = requests.get(url, params = params, headers = headers)
+    print 'verify_zip result: '+ res.text
+    if res.status_code == requests.codes.ok:
+        verified = True
+    return verified
+
 
 
 
