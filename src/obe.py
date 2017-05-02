3e9bf88e20804dd00c03247764c5c83e62a50915
Eugene Lin <eugenelin@Eugenes-MacBook-Pro.local>
2017-05-01 23:35:09 -0700

    debug

--- src/obe.py
+++ src/obe.py
@@ -4,7 +4,7 @@ def is_zip_verified(zip_code):
     # Authenticate
     verified = False
     auth = authenticate()
-    if auth['error']:
+    if 'error' in auth.keys():
         return verified
     access_token = auth['access_token']
     instance_url = auth['instance_url']
@@ -13,7 +13,7 @@ def is_zip_verified(zip_code):
         "Authorization":'Bearer '+access_token
     }
     params = {
-        'from_postal_code':zip_code, 
+        'from_postal_code':zip_code,
         'brand':os.environ['OBE_BRAND']
     }
     res = requests.get(url, params = params, headers = headers)
