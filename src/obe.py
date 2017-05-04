8ad92b1d15a8fdd416f1b4bdacf9b62b50dac5d4
Eugene Lin <eugenelin@adminlocalsmbp.1800gotjunk.com>
2017-05-04 14:05:22 -0700

    obe authenticate at init

--- src/obe.py
+++ src/obe.py
@@ -7,14 +7,16 @@ class OBE(object):
         self.instance_url = None
         self.franchise_id = None
         self.zipcode = None
+        self.authenticated = False
+        self.__authenticate()
 
     def is_zip_verified(self, zipcode):
         # Authenticate
 
         if not zipcode:
             return {'error':'zipcode is None'}
-        is_authenticated = self.__authenticate()
-        if not is_authenticated:
+
+        if not self.__authenticate():
             return {'error':'OBE Authentication error'}
 
         url = self.instance_url + os.environ['OBE_RESOURCE_PATH_VERIFY_AREA']
@@ -37,6 +39,8 @@ class OBE(object):
 
     def get_availabilities(self):
         print 'get_availability()'
+        if not self.__authenticate():
+            return {'error':'OBE Authentication error'}
         url = self.instance_url + os.environ['OBE_RESOURCE_PATH_AVAILABILITY']
         start_date = datetime.date.today().isoformat()
         end_date = (datetime.date.today() + datetime.timedelta(days=2)).isoformat()
@@ -60,21 +64,27 @@ class OBE(object):
             print 'error occurred in get_availabilities: '+res.text
             return {'error':res.text}
 
+    def hold_timeslot(self, start_time, finish_time):
+        print 'hold_timeslot(%s %s)'%(start_time, finish_time)
+        if not self.__authenticate():
+            return {'error':'OBE Authentication error'}
+
+
     def __authenticate(self):
         print 'authenticate with OBE'
-        result = False
-        url = os.environ['OBE_AUTH_URL']
-        data = {
-            'grant_type':'password',
-            'client_id': os.environ['OBE_CLIENT_ID'],
-            'client_secret': os.environ['OBE_CLIENT_SECRET'],
-            'username': os.environ['OBE_USERNAME'],
-            'password': os.environ['OBE_PASSWORD']
-        }
-        res_json = requests.post(url, data=data).json()
-        print 'authenticate result: ' + json.dumps(res_json, indent = 4)
-        if 'access_token' in res_json.keys() and 'instance_url' in res_json.keys():
-            self.access_token = res_json.get('access_token')
-            self.instance_url = res_json.get('instance_url')
-            result = True
-        return result
+        if not self.authenticated:
+            url = os.environ['OBE_AUTH_URL']
+            data = {
+                'grant_type':'password',
+                'client_id': os.environ['OBE_CLIENT_ID'],
+                'client_secret': os.environ['OBE_CLIENT_SECRET'],
+                'username': os.environ['OBE_USERNAME'],
+                'password': os.environ['OBE_PASSWORD']
+            }
+            res_json = requests.post(url, data=data).json()
+            print 'authenticate result: ' + json.dumps(res_json, indent = 4)
+            if 'access_token' in res_json.keys() and 'instance_url' in res_json.keys():
+                self.access_token = res_json.get('access_token')
+                self.instance_url = res_json.get('instance_url')
+                self.authenticated = True
+        return self.authenticated
