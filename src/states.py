0e508d02523ff45e3d0d2f5860c10f3d164696b3
Eugene Lin <eugenelin@adminlocalsmbp.1800gotjunk.com>
2017-05-04 13:05:29 -0700

    test

--- src/states.py
+++ src/states.py
@@ -99,10 +99,15 @@ class WAIT_FOR_TIMESLOT(State):
         starts = [dateutil.parser.parse(availabilities[i].get('start')) for i in range(len(availabilities)) ]
         print str(starts)
         needle = dateutil.parser.parse(timeslot)
-        pos = bisect.bisect_left(availabilities, needle) # will return index if found, else index of first later times
-        starts = starts[pos:]
+        # position of timeslot if found, else index of first time slot that is larger
+        pos = bisect.bisect_left(availabilities, needle)
+        if pos > len(starts): # sender seleted time bigger than all starting time. So we list backwards
+            starts = starts[len(starts)::-1]
+        else # include only times bigger or equal to sender selected time.
+            starts = starts[pos:]
         print str(starts)
         
+
 
     def __get_timeslot(datetime_str):
         pass
