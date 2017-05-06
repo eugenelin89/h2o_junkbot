833abacef49f9058a039b23faed1f07530334954
Eugene Lin <eugenelin@Eugenes-MacBook-Pro.local>
2017-05-06 01:20:41 -0700

    test

--- src/states.py
+++ src/states.py
@@ -76,8 +76,10 @@ class WAIT_FOR_ADDRESS(State):
         print json.dumps(address, indent=4)
         if not address.get('zip'):
             # Get it from Firebase
-            address['zip'] = self.get_order_with_key('zip')
-            print 'found zip!' + json.dumps(address, indent=4) + self.get_order_with_key('zip')
+            zip = self.get_order_with_key('zip')
+            address['zip'] = zip
+            print 'zip: ' + str(zip)
+            print 'found zip!' + json.dumps(address, indent=4)
         if not address.get('country'):
             # determine US or Canada based on the zip
             pattern = '[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]'
