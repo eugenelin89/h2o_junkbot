4db3b06dee943036f6f413f84a738d811ec563a7
Eugene Lin <eugenelin@adminlocalsmbp.1800gotjunk.com>
2017-05-08 16:05:45 -0700

    test

--- src/states.py
+++ src/states.py
@@ -152,7 +152,7 @@ class WAIT_FOR_EMAIL(State):
         self.send_messages([self.__format_confirmation(res)])
         qr = [{'content_type':'text', 'title':BOOK_JOB, 'payload':'BOOK_JOB'},{'content_type':'text', 'title':CANCEL, 'payload':'CANCEL'}]
         self.send_messages([PROCEED], quick_reply=qr)
-        self.set_next_state('RESET') # Debug
+        self.set_next_state('WAIT_FOR_CONFIRMATION') # Debug
 
     def __format_confirmation(self, order):
         # Name
