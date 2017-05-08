f3912ab0ba024c770be3e022801821cbd967034a
Eugene Lin <eugenelin@adminlocalsmbp.1800gotjunk.com>
2017-05-08 15:47:16 -0700

    updating msg

--- src/messages.py
+++ src/messages.py
@@ -1,4 +1,4 @@
-HELLO_MESSAGE_1 = 'I am O2E Junkbot. Glad to be of service!'
+HELLO_MESSAGE_1 = 'I am Junkbot. Glad to be of service!'
 HELLO_MESSAGE_2 = 'I am here to help you book junk removal service.'
 PROMPT_ZIP_MESSAGE = 'To check the service availability in your area, please let me know your ZIP code?'
 UNVERFIABLE_ZIP = 'We are unable to verify the ZIP code you provided... Please give us a call and a live agent will be able to help you book the service.'
