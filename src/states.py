488f7055b57d666a504a7d7bf2697ff72ad92c9c
Eugene Lin <eugenelin@adminlocalsmbp.1800gotjunk.com>
2017-05-08 09:50:56 -0700

    remove uneeded comments

--- src/states.py
+++ src/states.py
@@ -113,11 +113,6 @@ class WAIT_FOR_PHONE(State):
             print segment
             phone = phone + segment
         self.update_order({'phone':phone})
-        ## Getting confirmation info
-        #res = requests.get(os.environ['CONFIRM_URL'], {'sender_id' : self.sender_id}).json()
-        #self.send_messages([self.__format_confirmation(res)])
-        #qr = [{'content_type':'text', 'title':BOOK_JOB, 'payload':'BOOK_JOB'},{'content_type':'text', 'title':CANCEL, 'payload':'CANCEL'}]
-        #self.send_messages([PROCEED], quick_reply=qr)
         self._prompt_for_next_state()
         self.set_next_state('RESET') # Debug
 
